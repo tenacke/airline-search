509ac2d546d687be227011d5806d5f55638969d4
tenacke <emrekilic1010@gmail.com>
2023-12-09 11:22:09 +0300

    Removed data and added new cost function

--- src/Main.java
+++ src/Main.java
@@ -16,12 +16,14 @@ public class Main {
 
     public record PriorityObject(String id, long time, PriorityObject parent, double g, double h) { }
 
-    public record Mission(String startNode, long startTime, String endNode) { }
+    public record Mission(String startNode, String endNode, long startTime, long deadline) { }
 
-    static final String printFormat = "%d %d %d\n"; // id, time, cost
-    static final boolean dijkstra = true;  // Change this to false to use A* algorithm
+    static final String input = "AS-1";
+    static final boolean edges = true; // Change this to false to calculate edges
+    static final boolean dijkstra = false;  // Change this to false to use A* algorithm
     static final long sixHours = 21600; // 6 hours in seconds
-    static final int maxTime = 30; // Maximum time for the weather data
+    static final long minTime = 1680307200;
+    static final long maxTime = 1682899200; // Maximum time for the weather data
     String aircraftType; // Maximum edge distance in meters
     int edgeConstraint;
     int _18hourLimit;
@@ -39,13 +41,16 @@ public class Main {
     BufferedWriter edgesWriter;
     BufferedWriter pathWriter;
 
-    public Main() throws IOException { 
-    inputReader = new BufferedReader(new FileReader(new File("cases/data/turkey0.csv")));
-    dataReader = new BufferedReader(new FileReader(new File("cases/weather.csv")));
-    missionReader = new BufferedReader(new FileReader(new File("cases/flights/turkey0.in")));
-    edgesWriter = new BufferedWriter(new FileWriter(new File("edges.csv")));
-    edgeReader = new BufferedReader(new FileReader(new File("edges.csv")));
-    pathWriter = new BufferedWriter(new FileWriter(new File("path.out")));
+    public Main() throws IOException { // INITIALIZE READERS
+        inputReader = new BufferedReader(new FileReader(new File("cases/data/" + input + ".csv")));
+        dataReader = new BufferedReader(new FileReader(new File("cases/weather.csv")));
+        missionReader = new BufferedReader(new FileReader(new File("cases/flights/" + input + ".in")));
+        if (edges){
+            edgeReader = new BufferedReader(new FileReader(new File("cases/edges/" + input + ".csv")));
+            pathWriter = new BufferedWriter(new FileWriter(new File("path.out")));
+        }
+        else 
+            edgesWriter = new BufferedWriter(new FileWriter(new File("cases/edges/" + input + ".csv")));
     }
 
     private void readInput() throws IOException { // INITIALIZE NODES
@@ -96,26 +101,26 @@ public class Main {
         switch (aircraftType) {
             case "Carreidas 160":
                 edgeConstraint = 500;
-                _18hourLimit = 300;
-                _12hourLimit = 150;
+                _18hourLimit = 400;
+                _12hourLimit = 300;
                 break;
             
             case "Orion III":
-                edgeConstraint = 500;
-                _18hourLimit = 300;
-                _12hourLimit = 150;
+                edgeConstraint = 1000;
+                _18hourLimit = 750;
+                _12hourLimit = 500;
                 break;
 
             case "Skyfleet S570":
-                edgeConstraint = 500;
-                _18hourLimit = 300;
-                _12hourLimit = 150;
+                edgeConstraint = 1500;
+                _18hourLimit = 1200;
+                _12hourLimit = 900;
                 break;
 
             case "T-16 Skyhopper":
-                edgeConstraint = 500;
-                _18hourLimit = 300;
-                _12hourLimit = 150;
+                edgeConstraint = 2000;
+                _18hourLimit = 1500;
+                _12hourLimit = 1000;
                 break;
         }
     }
@@ -123,12 +128,15 @@ public class Main {
     private void readMissions() throws IOException { // READ MISSIONS
         String line;
         while ((line = missionReader.readLine()) != null) {
+            if (line.charAt(0) == '#')
+                continue;
             String[] data = line.split(" ");
             String startNode = data[0];
             String endNode = data[1];
-            // long startTime = Long.parseLong(data[2]);
+            long startTime = Long.parseLong(data[2]);
+            Long deadline = Long.parseLong(data[3]);
             // TODO - Add deadline and start time
-            missions.add(new Mission(startNode, 1680307200, endNode));
+            missions.add(new Mission(startNode, endNode, startTime, deadline));
         }
     }
 
@@ -143,6 +151,7 @@ public class Main {
         while (!queue.isEmpty()){
             PriorityObject current = queue.poll();
             visited.add(current.id + "_" + current.time);
+            // System.out.println(current.id + " " + current.time);
             if (current.id.equals(mission.endNode)) {
                 result = current;
                 break;
@@ -150,29 +159,37 @@ public class Main {
             Node currentNode = nodes.get(current.id);
             LinkedList<Node> adjacents = currentNode.adjacents;
             LinkedList<Double> distances = currentNode.distance;
-            if (!visited.contains(current.id + "_" + (current.time + sixHours)))
+            if ((current.time + sixHours <= mission.deadline) && !visited.contains(current.id + "_" + (current.time + sixHours)))
                 queue.add(new PriorityObject(current.id, current.time + sixHours, current, current.g + currentNode.waitCost, current.h));
             for (int i = 0; i < adjacents.size(); i++) {
                 Node adjacent = adjacents.get(i);
                 double distance = distances.get(i);
+                // System.out.println(adjacent.id + " " + distance);
                 long time = current.time + calculateTravelTime(distance);
-                if (time > maxTime)
-                    continue;
                 String key = adjacent.id + "_" + time;
-                if (!visited.contains(key)) {
+                if (time <= mission.deadline && !visited.contains(key)) {
                     double g = current.g + calculateCost(currentNode, adjacent, distance, current.time, time);
-                    double h = calculateHeuristic(adjacent, time);
+                    double h = calculateHeuristic(adjacent, mission.endNode, time);
                     queue.add(new PriorityObject(adjacent.id, time, current, g, h));
                 }
             }
         }
         if (result != null) {
-            while (result.parent != null) {
-                System.out.printf(printFormat, result.id, result.time, result.g);
+            double cost = 0d;
+            StringBuilder builder = new StringBuilder();
+            while (result != null) {
+                builder.append(result.id).append(" ");
+                cost += result.g;
                 result = result.parent;
             }
-        } else 
+            builder.append(cost);
+            pathWriter.write(builder.toString() + "\n");
+            System.out.println(builder.toString());
+        } else {
+            pathWriter.write("No feasible path\n");
             System.out.println("No feasible path");
+        }
+        pathWriter.flush();
     }
 
     private double calculateWeatherMultplier(int weather) {
@@ -188,11 +205,12 @@ public class Main {
     }
 
     private double calculateCost(Node node1, Node node2, double distance, long time1, long time2) {
+        int fixedCost = 300;
         int weather1 = airfields.get(node1.airfield + "_" + time1).weather;
         int weather2 = airfields.get(node2.airfield + "_" + time2).weather;
         double departureMultiplier = calculateWeatherMultplier(weather1);
         double arrivalMultiplier = calculateWeatherMultplier(weather2);
-        double result = distance * departureMultiplier * arrivalMultiplier;
+        double result = fixedCost * departureMultiplier * arrivalMultiplier + distance;
         return result;
     }
 
@@ -205,10 +223,11 @@ public class Main {
             return sixHours;
     }
 
-    private int calculateHeuristic(Node node, long time) {
+    private double calculateHeuristic(Node node, String target, long time) {
         if (dijkstra)
-            return 0;
-        return 0; // TODO - Implement this
+            return 0d;
+        Node targetNode = nodes.get(target);
+        return calculateDistance(node, targetNode); // TODO - Implement this
     }
 
     private void run() throws IOException {
@@ -273,7 +292,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
-        // main.runGraph();
-        main.run();
+        if (edges)
+            main.run();
+        else
+            main.runGraph();
     }
 }
